ae5c72b3bcc6240c5df93bb32a50ba92633b14b0
Mark <markmdvd@gmail.com>
2020-02-01 13:42:49 -0800

    Example hack.

---
+++ GlobalGameJam2020/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(0,0,0);
+    }
+
+    void LateUpdate()
+    {
+        float newXPosition = player.transform.position.x - offset.x;
+        float newZPosition = player.transform.position.z - offset.z;
+
+        transform.position = new Vector3(newXPosition, transform.position.y, newXPosition);
+    }
+}
---
+++ GlobalGameJam2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+
+     public float speed;                //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    public float jumpHeight;
+    public float jumps;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumps > 0)
+            {
+                rb2d.velocity = Vector2.up * jumpHeight;
+                jumps--;
+            }
+
+
+        }
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            jumpHeight *= 2;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
